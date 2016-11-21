06a4ade5f37819b151c28d88c051ef3cde09a91a
Morgane Grau <mogrba@gmail.com>
2016-11-21 10:07:24 +0100

    ajout exercice objets

---
+++ exos JS/objets.js
@@ -0,0 +1,41 @@
+$liste = document.getElementById("liste");
+$prenom = document.getElementById("prenom");
+$nom = document.getElementById("nom");
+$numero = document.getElementById("numero");
+$ajouter = document.getElementById("ajouter");
+
+contacts = [{
+    prenom: "Momo",
+    nom: "Grillot",
+    numero: "0123456789"
+}, {
+    prenom: "Riri",
+    nom: "Smith",
+    numero: "0123456789"
+}];
+function texteContact(contact) {
+return contact.prenom + " " + contact.nom + " (" + contact.numero + ")";
+}
+alert(texteContact(contacts[0])); // affiche « Julien Grillot (0123456789) »
+
+function afficher() {
+    elements = "";
+    for (i = 0; i < contacts.length; i++) {
+        elements += "<li>" + texteContact(contacts[i]) + "</li>";
+    }
+    $liste.innerHTML = elements;
+}
+
+
+
+function ajout(contact){
+    contacts.push({
+        prenom: $prenom.value,
+        nom: $nom.value,
+        numero: $numero.value
+    });
+    afficher();
+}
+
+$ajouter.onclick = ajout;
+afficher();
